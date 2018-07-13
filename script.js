88e774fcdd2d06e4d308c80ade707c87762f64dd
Nattapon Benz <sevipsagis@gmail.com>
2018-07-13 12:19:31 +0700

    add condition if player insert same number

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+var numberArray = [];
+for(let i=1; i<100; i++){numberArray.push(i);}
+console.log(numberArray);
 $(document).ready(() => {
     var inputBox = $(".inputBox");
     inputBox.on("click", function () {
         if (this.value == "") {
-            let playerNumber = prompt("Enter your number");
-            this.value = playerNumber;
+            let playerNumber = parseInt(prompt("Enter your number"));
+            if(numberArray[playerNumber-1] != null){
+                this.value = playerNumber;
+                numberArray[playerNumber-1] = null;
+                console.log(numberArray);
+            }
+            else{this.value = "";}
         }
     });
 });
